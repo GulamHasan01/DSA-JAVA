9b55ce20e3f75f8ad2f9d1c9dc4975ac7bcd2537
Gulam Hasan <ghulamhasan665@gmail.com>
2026-04-17 14:12:38 +0530

    Added: Reverse words in-place and using extra space for each word

---
+++ String/ReverseWord.java
@@ -0,0 +1,65 @@
+package String;
+
+import java.util.ArrayList;
+import java.util.List;
+// Reverse words in-place and using extra space for each word
+
+public class ReverseWord {
+    public static void main(String[] args) {
+        System.out.println(revWord("hello gulam hasan"));
+    }
+    public static String revWord(String s){
+        List<String>list=new ArrayList<>();
+        StringBuilder ans= new StringBuilder();
+          int j=s.length();
+        for (int i = s.length()-1; i >=0 ; i--) {
+
+            if (s.charAt(i)==' '){
+                list.add(s.substring(i+1,j));
+                j=i;
+            }
+
+        }
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i)==' '){
+                list.add(s.substring(0,i));
+                break;
+            }
+        }
+        for (int i = 0; i < list.size() ; i++) {
+            ans.append(list.get(i));
+            if (i!=list.size()-1) ans.append(' ');
+        }
+        return ans.toString();
+    }
+
+    public static String revWord1(String s) {
+        char[] arr = s.toCharArray();
+
+        // 1. reverse whole string
+        reverse(arr, 0, arr.length - 1);
+
+        int start = 0;
+
+        // 2. reverse each word
+        for (int end = 0; end <= arr.length; end++) {
+            if (end == arr.length || arr[end] == ' ') {
+                reverse(arr, start, end - 1);
+                start = end + 1;
+            }
+        }
+
+        return new String(arr);
+    }
+
+    private static void reverse(char[] arr, int l, int r) {
+        while (l < r) {
+            char temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
