57a8510e0f71834f8eb6f5d9672bdd256d3b9179
Gulam Hasan <ghulamhasan665@gmail.com>
2026-04-20 22:22:45 +0530

    Added  Finds the lowest common ancestor of two nodes using recursive DFS by checking left and right subtrees.

--- Trees/LowestCommonAncestor.java
+++ Trees/LowestCommonAncestor.java
@@ -1,4 +1,21 @@
 package Trees;
 
+//  Finds the lowest common ancestor of two nodes using recursive DFS by checking left and right subtrees.
 public class LowestCommonAncestor {
+
+
+    private static TreeNode lca(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lca(root.left, p, q);
+        TreeNode right = lca(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+        return (left != null) ? left : right;
+    }
 }
