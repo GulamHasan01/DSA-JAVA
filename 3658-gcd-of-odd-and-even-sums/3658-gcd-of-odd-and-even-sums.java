02c6401126c8992208f84590e151e1f66ad23b55
Gulam Hasan <140590825+GulamHasan01@users.noreply.github.com>
2026-07-15 18:40:38 +0530

    Time: 0 ms (100%), Space: 42.5 MB (74.78%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,18 +1,5 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int evsum=0,oddsum=0;
-
-        for(int i=1;i<=2*n;i++){
-            if(i%2==0) evsum+=i;
-            else oddsum+=i;
-        }
-        int max=0;
-        int min=Math.min(evsum,oddsum);
-        for(int i=1;i<=min;i++){
-      if(evsum%i==0 && oddsum%i==0){
-        max=i;
-      }
-        }
-         return max;
-    }
+    return n;
+}
 }
