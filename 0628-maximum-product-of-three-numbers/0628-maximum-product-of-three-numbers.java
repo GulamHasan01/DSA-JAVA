e0bd1fada611d7d50db1c10f67789e8ad915f653
Gulam Hasan <140590825+GulamHasan01@users.noreply.github.com>
2026-07-26 13:23:55 +0530

    Time: 16 ms (37.76%), Space: 48 MB (70.8%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,24 +1,16 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        Arrays.sort(nums);
-        
-        if (nums[0] >= 0 || nums[nums.length - 1] <= 0 || nums.length == 3) {
-            return nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
-        } else if (nums[1] <= 0 && nums[nums.length - 1] >= 0) {
-            int neg = nums[0] * nums[1];
-            int pos = nums[nums.length - 1] * nums[nums.length - 2];
-            if (neg * nums[nums.length - 1] > pos * nums[nums.length - 3])
-                return neg * nums[nums.length - 1];
-            else
-                return pos * nums[nums.length - 3];
-            // if (pos > neg) {
-            //     if (nums[nums.length - 3] > 0)
-            //         return nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
-            //         else  return neg * nums[nums.length - 1];
-            // } else {
-            //     return neg * nums[nums.length - 1];
-            // }
-        }
-        return -1;
+      Arrays.sort(nums);
+
+int option1 = nums[nums.length - 1] *
+              nums[nums.length - 2] *
+              nums[nums.length - 3];
+
+int option2 = nums[0] *
+              nums[1] *
+              nums[nums.length - 1];
+
+return Math.max(option1, option2);
+     
     }
 }
