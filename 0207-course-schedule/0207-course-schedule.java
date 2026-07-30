185d1234190263dae2388bfb660841a9877dd0e6
Gulam Hasan <140590825+GulamHasan01@users.noreply.github.com>
2026-07-30 12:28:00 +0530

    Time: 4 ms (91.37%), Space: 47.3 MB (21.6%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,37 +1,41 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Queue<Integer> q = new LinkedList<>();
-        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
-        HashMap<Integer, ArrayList<Integer>> outmap = new HashMap<>();
-        for (int[] num : prerequisites) {
-            map.computeIfAbsent(num[0], k -> new ArrayList<>()).add(num[1]);
-            outmap.computeIfAbsent(num[1], k -> new ArrayList<>()).add(num[0]);
-        }
+   
+        List<Integer>[] graph = new ArrayList[numCourses];
         for (int i = 0; i < numCourses; i++) {
-            if (!map.containsKey(i)) {
-                q.offer(i);
+            graph[i] = new ArrayList<>();
+        }
+        
+        int[] indegree = new int[numCourses];
+        
+        for (int[] prereq : prerequisites) {
+            graph[prereq[1]].add(prereq[0]);
+            indegree[prereq[0]]++;
+        }
+        
+       
+        int[] queue = new int[numCourses];
+        int front = 0, rear = 0;
+        
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue[rear++] = i;
             }
         }
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                int curr = q.poll();
-                if (outmap.containsKey(curr)) {
-                    ArrayList<Integer> list = outmap.get(curr);
-                    for (int num : list) {
-                        map.get(num).remove(Integer.valueOf(curr));
-                        if (map.containsKey(num) && map.get(num).size() == 0) {
-                            q.offer(num);
-                            map.remove(num);
-                        }
-                    }
-
+        
+        int completed = 0;
+        
+        while (front < rear) {
+            int course = queue[front++];
+            completed++;
+            
+            for (int nextCourse : graph[course]) {
+                if (--indegree[nextCourse] == 0) {
+                    queue[rear++] = nextCourse;
                 }
             }
         }
-        if (map.isEmpty())
-            return true;
-
-        return false;
+        
+        return completed == numCourses;
     }
 }
