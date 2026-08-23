b941acf89dc1040841753c3c20429dc273c34e0f
Gulam Hasan <140590825+GulamHasan01@users.noreply.github.com>
2026-08-23 13:01:32 +0530

    Time: 25 ms (82.1%), Space: 44.1 MB (93.51%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,22 +1,37 @@
 class Solution {
-    private boolean isPerf(int n){
-        int sq=(int)(Math.sqrt(n));
-        return sq*sq==n;
+    private boolean isPerf(int n) {
+        int sq = (int) (Math.sqrt(n));
+        return sq * sq == n;
     }
+
     public int numSquares(int n) {
-        int[]dp=new int[n+1];
-       Arrays.fill(dp,-1);
-        return minSqrt(n,dp);
+        int[] dp = new int[n + 1];
+        for (int j = 1; j <= n; j++) {
+            if (isPerf(j))
+               dp[j] = 1;
+            else {
+                int min = Integer.MAX_VALUE;
+                for (int i = 1; i * i <= j; i++) {
+                    int count = dp[i * i] +dp[j-i * i];
+                    min = Math.min(min, count);
+                }
+               dp[j] = min;
+            }
+        }
+        return dp[n];
 
     }
-    private int minSqrt(int n,int[]dp){
-        if(isPerf(n)) return 1;
-          if(dp[n]!=-1) return dp[n];
-          int min=Integer.MAX_VALUE;
-        for(int i=1;i*i<=n;i++){
-            int count=minSqrt(i*i,dp)+minSqrt(n-i*i,dp);
-            min=Math.min(min,count);
-        }
-          return dp[n]=min;
-    }
+
+    // private int minSqrt(int n, int[] dp) {
+    //     if (isPerf(n))
+    //         return 1;
+    //     if (dp[n] != -1)
+    //         return dp[n];
+    //     int min = Integer.MAX_VALUE;
+    //     for (int i = 1; i * i <= n; i++) {
+    //         int count = minSqrt(i * i, dp) + minSqrt(n - i * i, dp);
+    //         min = Math.min(min, count);
+    //     }
+    //     return dp[n] = min;
+    // }
 }
