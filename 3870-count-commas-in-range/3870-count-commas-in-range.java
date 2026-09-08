b204104b73049de9fe860c7faa414567d7bd57d9
Gulam Hasan <140590825+GulamHasan01@users.noreply.github.com>
2026-09-08 10:16:44 +0530

    Time: 1 ms (83.65%), Space: 42.3 MB (85.26%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countCommas(int n) {
+        String s=String.valueOf(n);
+        if(s.length()<4) return 0;
+        else{
+            n=n-999;
+            return n;
+        }
+    }
+    
+}
